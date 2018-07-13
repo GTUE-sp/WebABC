a2564828b54f5281c64e9f0f106bbb1738a72b79
youta ogino <dev.youta1119@gmail.com>
2018-07-13 13:32:23 +0900

    inteoduce react

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  singleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  parser: 'typescript'
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const history = require('connect-history-api-fallback');
+const convert = require('koa-connect');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+    mode: process.env.WEBPACK_SERVE ? "development" : "production",
+    entry: [
+        path.join(__dirname, './src/index.tsx')
+    ],
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(tsx?)|(js)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    serve: {
+        port: 8000,
+        hot: true,
+        add: (app, middleware, options) => {
+            const historyOptions = {};
+            app.use(convert(history(historyOptions)));
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "template.html",
+            inject: "div"
+        }),
+        new ForkTsCheckerWebpackPlugin()
+    ]
+}
