3640761dc21b2c866e1f96a21ac6e78ca862bd6a
youta ogino <dev.youta1119@gmail.com>
2018-11-23 06:35:37 +0900

    add appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import { BrowserRouter, Route } from "react-router-dom";
+//import { AttendanceBook } from "./AttendanceBook";
+import { AppBar, Button, Input, Switch, Toolbar, Typography } from "@material-ui/core";
 import { AttendanceBook } from "./AttendanceBook";
-
-export const App: React.SFC = () => {
-  return (
-    <BrowserRouter>
+/**
+ * <BrowserRouter>
       <div>
         <Route exact={true} path="/" component={AttendanceBook} />
       </div>
     </BrowserRouter>
+ */
+export const App: React.SFC = () => {
+  return (
+    <div>
+      <AppBar position={"static"}>
+        <Toolbar>
+          <Typography variant={"title"} color={"inherit"}>
+            Web出席簿
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <AttendanceBook />
+    </div>
   );
 };
--- src/AttendanceBookTable.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Table } from "antd";
-import "antd/lib/table/style/css";
-export interface IUser {
-  key: number;
-  name: string;
-}
-
-export class AttendanceBookTable extends Table<IUser> {}
