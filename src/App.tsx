f00fee7ba6411a532b9ee227cbd75f4bd5418363
youta ogino <dev.youta1119@gmail.com>
2018-11-23 11:47:07 +0900

    sticky header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import { BrowserRouter, Route } from "react-router-dom";
-//import { AttendanceBook } from "./AttendanceBook";
+
 import { AppBar, Button, Input, Switch, Toolbar, Typography } from "@material-ui/core";
-import { AttendanceBook } from "./AttendanceBook";
-/**
- * <BrowserRouter>
+import AttendanceBook from "./AttendanceBook";
+/*
+ <BrowserRouter>
       <div>
         <Route exact={true} path="/" component={AttendanceBook} />
       </div>
     </BrowserRouter>
- */
+*/
+
 export const App: React.SFC = () => {
   return (
     <div>
@@ -21,9 +22,7 @@ export const App: React.SFC = () => {
           </Typography>
         </Toolbar>
       </AppBar>
-      <div>
-        <AttendanceBook />
-      </div>
+      <AttendanceBook />
     </div>
   );
 };
--- src/AttendanceBook.tsx
+++ src/AttendanceBook.tsx
@@ -1,157 +1,171 @@
 // tslint:disable:max-classes-per-file
 import React from "react";
-import { Table } from "antd";
-import "antd/lib/table/style/css";
-import { ColumnProps } from "antd/lib/table";
-
-export interface Student {
-  key: number;
-  name: string;
-}
-
-class AttendanceBookTable extends Table<Student> {}
-
-export class AttendanceBook extends React.Component<{}, {}> {
-  private columns: Array<ColumnProps<Student>> = [
-    {
-      key: "name",
-      title: "名前",
-      dataIndex: "name",
-      fixed: "left",
-      width: 200,
-      render: (text, recoed, index) => {
-        return (
-          <span>
-            {" "}
-            {recoed.key} {recoed.name}
-          </span>
-        );
-      }
+import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+const styles = (theme: Theme) => {
+  return createStyles({
+    root: {
+      width: "100%",
+      marginTop: theme.spacing.unit * 3,
+      overflowX: "auto"
     },
-    {
-      key: "period",
-      title: "時限",
-      children: [
-        {
-          key: "period1",
-          title: "1",
-          render: () => {
-            return (
-              <p>
-                出欠
-                <input type="checkbox" />
-              </p>
-            );
-          }
-        },
-        {
-          key: "period2",
-          title: "2",
-          render: () => {
-            return (
-              <p>
-                出欠
-                <input type="checkbox" />
-              </p>
-            );
-          }
-        },
-        {
-          key: "period3",
-          title: "3",
-          render: () => {
-            return (
-              <p>
-                出欠
-                <input type="checkbox" />
-              </p>
-            );
-          }
-        },
-        {
-          key: "period4",
-          title: "4",
-          render: () => {
-            return (
-              <p>
-                出欠
-                <input type="checkbox" />
-              </p>
-            );
-          }
-        },
-        {
-          key: "period5",
-          title: "5",
-          render: () => {
-            return (
-              <p>
-                出欠
-                <input type="checkbox" />
-              </p>
-            );
-          }
-        },
-        {
-          key: "period6",
-          title: "6",
-          render: () => {
-            return (
-              <p>
-                出欠
-                <input type="checkbox" />
-              </p>
-            );
-          }
-        },
-        {
-          key: "period7",
-          title: "7",
-          render: () => {
-            return (
-              <p>
-                出欠
-                <input type="checkbox" />
-              </p>
-            );
-          }
-        },
-        {
-          key: "period8",
-          title: "8",
-          render: () => {
-            return (
-              <p>
-                出欠
-                <input type="checkbox" />
-              </p>
-            );
-          }
-        }
-      ]
+    head: {
+      backgroundColor: "#fff",
+      position: "sticky",
+      top: 0
+    },
+    table: {
+      border: 1
     }
-  ];
+  });
+};
 
-  private data: Student[] = [];
+interface Data {
+  id: number;
+  name: string;
+  calories: number;
+  fat: number;
+  carbs: number;
+  protein: number;
+}
+let id = 0;
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number): Data {
+  id += 1;
+  return { id, name, calories, fat, carbs, protein };
+}
+const data = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9)
+];
 
-  constructor(props: {}) {
+type AttendanceBookProps = WithStyles<typeof styles>;
+export class AttendanceBook extends React.Component<AttendanceBookProps, {}> {
+  constructor(props: AttendanceBookProps) {
     super(props);
-    for (let i = 1; i < 53; i++) {
-      this.data.push({
-        key: i,
-        name: "キズナアイ"
-      });
-    }
+    console.log(props);
   }
   public render() {
+    const { classes } = this.props;
     return (
-      <AttendanceBookTable
-        dataSource={this.data}
-        columns={this.columns}
-        bordered={true}
-        pagination={false}
-        scroll={{ x: 1000 }}
-      />
+      <div style={{ height: "100%", width: "100%", overflowY: "scroll", position: "absolute" }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.head}>Dessert (100g serving)</TableCell>
+              <TableCell className={classes.head} numeric={true}>
+                Calories
+              </TableCell>
+              <TableCell className={classes.head} numeric={true}>
+                Fat (g)
+              </TableCell>
+              <TableCell className={classes.head} numeric={true}>
+                Carbs (g)
+              </TableCell>
+              <TableCell className={classes.head} numeric={true}>
+                Protein (g)
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((n) => {
+              return (
+                <TableRow key={n.id}>
+                  <TableCell>{n.name}</TableCell>
+                  <TableCell numeric={true}>{n.calories}</TableCell>
+                  <TableCell numeric={true}>{n.fat}</TableCell>
+                  <TableCell numeric={true}>{n.carbs}</TableCell>
+                  <TableCell numeric={true}>{n.protein}</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
     );
   }
 }
+
+export default withStyles(styles)(AttendanceBook);
